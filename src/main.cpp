4601da625944ef2f131b60b6583503b679d7e7bb
孙善明 <sunshanming@zhuojianchina.com>
2018-08-02 17:50:18 +0800

    add

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,15 @@
 #include <SimpleDHT.h>
 #include <EEPROM.h>
 #include <SoftwareSerial.h>
-#include <ArduinoJson.h>
+
+#define RX 3
+#define TX 1
+#define DHT11 4
 
 SimpleDHT11 dht11;
 
-const int RX = 2;
-const int TX = 3;
-const int DHT11 = 4;
-
 SoftwareSerial swSer(RX, TX, false, 256);
-byte open[5] = {0xA1, 0xF1,0x01,0xFB,0x08};
+byte open[300] = {0xA1, 0xF1,0x01,0xFB,0x08};
 
 class WifiConfig : public Configuration
 {
@@ -57,7 +56,49 @@ int r6 = sprintf(node_switch_state_topic, "esp/%08X/ac/state", ESP.getChipId());
 
 char node_th_topic[30] = "";
 int r8 = sprintf(node_switch_state_topic, "esp/%08X/TH", ESP.getChipId());
+
+char node_10_control_topic[30] = "";
+int r10 = sprintf(node_10_control_topic, "esp/%08X/10/control", ESP.getChipId());
+char node_16_control_topic[30] = "";
+int r16 = sprintf(node_16_control_topic, "esp/%08X/16/control", ESP.getChipId());
 ///////////////////////////
+
+void ShowCommand(int index)  {
+  Serial.print(millis());
+  Serial.print(" OUT>>");
+  for (int i = 0; i < index; i++) {
+    Serial.print( (open[i] < 0x10 ? " 0" : " "));
+    Serial.print(open[i], HEX);
+  }
+  Serial.println();
+}
+
+void checkReturn() {
+  if (swSer.available()) {
+    Serial.print(millis());
+    Serial.print(" IN>>>");
+    while (swSer.available()) {
+      byte ch =  (byte) swSer.read();
+      Serial.print((ch < 0x10 ? " 0" : " "));
+      Serial.print(ch, HEX);
+    }
+    Serial.println();
+  }
+}
+
+void SendCommand(bool checkResult, int index) {
+  ShowCommand(index);
+  //串口
+  swSer.begin(9600);
+  pinMode(RX, INPUT);
+  pinMode(TX, OUTPUT);
+
+ 
+  swSer.write(open, index);
+  swSer.flush();
+  if (checkResult) checkReturn();
+}
+
 WiFiClient espClient;
 PubSubClient client(espClient);
 void callback(char *topic, byte *payload, unsigned int length)
@@ -66,38 +107,53 @@ void callback(char *topic, byte *payload, unsigned int length)
   Serial.print("msg from topic: ");
   Serial.print(topic);
   Serial.print(" payload: ");
-  String msg = "";
-  for (int i = 0; i < length; i++)
-  {
-    msg += (char)payload[i];
+
+  Serial.println();
+  char msg[length + 1];
+  for (int i = 0; i < length; i++) {
+    msg[i] = (char)payload[i];
   }
-  Serial.println(msg);
-  if (_topic == node_switch_control_topic)
-  {
-    Serial.print("switch control  ");
-    
+  msg[length] = '\0';
+  String msgText(msg);
+  Serial.print(msgText);
+  Serial.println();
+
+  if (_topic == node_10_control_topic || _topic == node_16_control_topic) {
+    char delimiter[] = " ";
+    char* command = strtok(msg,  delimiter);
+    int index = 0;
+    while (command != NULL) {
+      if (_topic == node_10_control_topic) {
+        open[index] = strtol(command, NULL, 10);
+      }else {
+        open[index] = strtol(command, NULL, 16);
+      }
+      
+      command = strtok(NULL, delimiter);
+      index ++;
+    }
+    SendCommand(true, index);
   }
-  else if (_topic == node_heart_topic)
-  {
-    int newval = msg.toInt() + 8;
-    char *_newval;
-    sprintf(_newval, "%d", newval);
-    client.publish(node_status_topic, _newval);
-  }
+
 }
 
 long lastReconnectAttempt = 0;
-long lastScheduleTime = 0;
+long lastScheduleTime500 = millis();
+long lastScheduleTime2000 = millis();
+long lastScheduleTime5000 = millis();
 int count = 0;
 const char *user = (&C.mqtt.user) -> c_str();
 const char *pass = (&C.mqtt.pass) -> c_str();
 void setup()
 {
   Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
   Serial.printf("Flash real id:");
   Serial.println(nodeid);
   
-  //串口
   swSer.begin(9600);
   pinMode(RX, INPUT);
   pinMode(TX, OUTPUT);
@@ -138,42 +194,19 @@ boolean reconnect()
     client.publish(node_status_topic, "online");
     Serial.println(node_status_topic);
     client.subscribe(node_switch_control_topic);
+    client.subscribe(node_10_control_topic);
+    client.subscribe(node_16_control_topic);
     Serial.println(node_switch_control_topic);
+    Serial.println(node_10_control_topic);
+    Serial.println(node_16_control_topic);
   }
   delay(100);
   return client.connected();
 }
 
-void ShowCommand()  {
-  Serial.print(millis());
-  Serial.print(" OUT>>");
-  for (int i = 0; i < 5; i++) {
-    Serial.print( (open[i] < 0x10 ? " 0" : " "));
-    Serial.print(open[i], HEX);
-  }
-  Serial.println();
-}
 
-void checkReturn() {
-  unsigned long startMs = millis();
-  while ( ((millis() - startMs) < 500) && (!swSer.available()) ) ;
-  if (swSer.available()) {
-    Serial.print(millis());
-    Serial.print(" IN>>>");
-    while (swSer.available()) {
-      byte ch =  (byte) swSer.read();
-      Serial.print((ch < 0x10 ? " 0" : " "));
-      Serial.print(ch, HEX);
-    }
-    Serial.println();
-  }
-}
 
-void SendCommand(bool checkResult) {
-  ShowCommand();
-  swSer.write(open, 5);
-  if (checkResult) checkReturn();
-}
+
 
 
 byte lastTemperature = 0;
@@ -190,14 +223,14 @@ void publishTemp() {
   Serial.println(" H");
 
   //mqtt publish
-  StaticJsonBuffer<50> jsonBuffer;
-  JsonObject& root = jsonBuffer.createObject();
-  root["temperature"] = (int)temperature;
-  root["thumidityime"] = (int)humidity;
-  String output;
-  root.printTo(output);
-  const char *res = output.c_str();
-  client.publish(node_th_topic, res);
+  // StaticJsonBuffer<50> jsonBuffer;
+  // JsonObject& root = jsonBuffer.createObject();
+  // root["temperature"] = (int)temperature;
+  // root["thumidityime"] = (int)humidity;
+  // String output;
+  // root.printTo(output);
+  // const char *res = output.c_str();
+  // client.publish(node_th_topic, res);
 
 
 
@@ -206,21 +239,29 @@ void publishTemp() {
 void schedule500() {
   long now = millis();
 
-  if ((now - lastScheduleTime) < 500) return;
-  lastScheduleTime = now;
+  if ((now - lastScheduleTime500) < 500) return;
+  lastScheduleTime500 = now;
   // 
-  publishTemp();
+  Serial.println("schedule500");
 
 
 }
 
 void schedule2000() {
   long now = millis();
-  if ((now - lastScheduleTime) < 2000) return;
-  lastScheduleTime = now;
+  if ((now - lastScheduleTime2000) < 2000) return;
+  lastScheduleTime2000 = now;
   // 
+  Serial.println("schedule2000");
   
-  
+}
+
+void schedule5000() {
+  long now = millis();
+  if ((now - lastScheduleTime5000) < 5000) return;
+  lastScheduleTime5000 = now;
+  // 
+  Serial.println("schedule5000");
 }
 
 
@@ -236,15 +277,18 @@ void connectMqtt() {
       }
     }
   }else {
-    client.loop();
     schedule2000();
     schedule500();
+    schedule5000();
+    client.loop();
+    
   }
 }
 
 
 void loop()
 {
+  
   Bleeper.handle();
   auto status = WiFi.status();
   bool isConnected = (status == WL_CONNECTED);
